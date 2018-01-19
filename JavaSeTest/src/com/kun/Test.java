0946acddd2e0272140618edaa671888f474069c0
hahah <2346543654@163.com>
2018-01-19 15:57:00 +0800

    初始化提交

---
+++ JavaSeTest/src/com/kun/Test.java
@@ -0,0 +1,13 @@
+package com.kun;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
