e46d85c3a294c3de1f00d50d4ddc7a5fb925028a
hahah <2346543654@163.com>
2018-01-19 16:22:10 +0800

    输了修改提交

--- JavaSeTest/src/Main.java
+++ JavaSeTest/src/Main.java
@@ -2,9 +2,7 @@
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println(43534);
-		System.out.println(43534);
-		System.out.println(43534);
+		System.out.println("一个简单的输出");
 	}
 
 }
